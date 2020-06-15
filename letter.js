13528541b9aad4e8c9f9fb065500b85830ade83b
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-06-14 20:22:45 -0400

    setting/checking characters

---
+++ letter.js
@@ -0,0 +1,18 @@
+const Letter = function(char) {
+    this.char = char;
+    this.used = false;
+    this.guessed = function() {
+        if (this.used){
+            return char
+        }
+        else {
+            return "_";
+        }
+    }
+
+   this.input = function(userInput) {
+       if (userInput === char){
+           this.used = true;
+       }
+   }
+}
